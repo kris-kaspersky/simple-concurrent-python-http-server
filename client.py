7abdc1ae00040375bc482754e940916819148368
starcroce <starcroce@yahoo.com>
2014-03-23 16:01:23 -0600

    can handle GET file, TODO multithread GET test

--- client.py
+++ client.py
@@ -1,14 +1,28 @@
 import sys
-import urllib2
-
+import httplib
 from threading import Thread
 
-def make_request(url):
-    print urllib2.urlopen(url).read()
+IP_ADDRESS = sys.argv[1]
 
-def main():
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
-    for _ in range(10):
-        Thread(target=make_request, args=("http://localhost:%d" % port,)).start()
+def make_request():
+	# print urllib2.urlopen(url).read()
+	connection = httplib.HTTPConnection(IP_ADDRESS)
+	while True:
+		command = raw_input('input command: ')
+		command = command.split()
+		if command[0] == 'exit':
+			break
+		connection.request(command[0], command[1])
+		rsp = connection.getresponse()
+		data_received = rsp.read()
+		print data_received
+	connection.close()
 
-main()
+# def main():
+# 	port = 8000
+# 	for _ in range(10):
+# 		Thread(target = make_request, args = ("http://localhost:%d" % port,)).start()
+
+# main()
+
+make_request()
--- server.py
+++ server.py
@@ -1,29 +1,45 @@
 import time
-from BaseHTTPServer   import BaseHTTPRequestHandler, HTTPServer, test as _test
-from SocketServer     import ThreadingMixIn
+import os
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer, test as _test
+from SocketServer import ThreadingMixIn
 
-# sequential server
-# class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-#     pass
+HOST_NAME = 'localhost'
+PORT_NUMBER = 80
 
 # concurrent server
-class ThreadedHTTPServer(HTTPServer):
-    pass 
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+	pass
 
-class SlowHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
+# sequential server
+# class ThreadedHTTPServer(HTTPServer):
+#     pass 
 
-        self.wfile.write("Entered GET request handler")
-        time.sleep(1)
-        self.wfile.write("Sending response!")
+class MyHandler(BaseHTTPRequestHandler):
+	def do_HEADS(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text-html")
+		self.end_headers()
 
-def test(HandlerClass = SlowHandler,
-         ServerClass = ThreadedHTTPServer):
-    _test(HandlerClass, ServerClass)
+	def do_GET(self):
+		file_dir = 'C:/Dropbox/Courses/Concurrent Programming/project/webserver/files/'
+		f = open(file_dir + self.path)
+		self.send_response(200)
+		self.send_header("Content-type", "text-html")
+		self.end_headers()
+		# self.wfile.write("Entered GET request handler --- ")
+		self.wfile.write(f.read())
+		time.sleep(1)
+		self.wfile.write("Sending response!")
 
+# test(HandlerClass=<class BaseHTTPServer.BaseHTTPRequestHandler>, ServerClass=<class BaseHTTPServer.HTTPServer>)
+# Test the HTTP request handler class. This runs an HTTP server on port 8000
+# def test(HandlerClass = SlowHandler, ServerClass = ThreadedHTTPServer):
+# 	_test(HandlerClass, ServerClass)
 
 if __name__ == '__main__':
-    test()
+	# test()
+	server_class = ThreadedHTTPServer
+	# if want to test the sequential http server
+	# server_class = HTTPServer
+	my_server = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+	my_server.serve_forever()

42722657c65c1023c3fa74bb478da3c3c276abeb
starcroce <starcroce@yahoo.com>
2014-04-29 17:14:32 -0600

    final submit version

--- test cases/client_2.py
+++ test cases/client_2.py
@@ -8,10 +8,8 @@ from multiprocessing import Process
 IP_ADDRESS = 'localhost'
 flights = ['UA001', 'UA002', 'UA003', 'UA004', 'UA005']
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
-# types = ['Query', 'Order']
 
 def make_query_request():
-	# print urllib2.urlopen(url).read()
 	connection = httplib.HTTPConnection(IP_ADDRESS, 8080)
 	# create random query
 	rand1, rand2 = randrange(0, 5), randrange(0, 5)
@@ -21,24 +19,18 @@ def make_query_request():
 	connection.request(command[0], command[1])
 	rsp = connection.getresponse()
 	data_received = rsp.read()
-	# print data_received
-	# print 'received'
 	connection.close()
 
 def make_order_request():
-	# print urllib2.urlopen(url).read()
 	connection = httplib.HTTPConnection(IP_ADDRESS, 8080)
 	# create random query
 	rand1, rand2 = randrange(0, 5), randrange(0, 5)
 	query = 'Order-' + flights[rand1] + '-' + days[rand2]
-	# query = 'Order-' + flights[0] + '-' + days[0]
 	command = 'GET ' + query
 	command = command.split()
 	connection.request(command[0], command[1])
 	rsp = connection.getresponse()
 	data_received = rsp.read()
-	# print data_received
-	# print 'received'
 	connection.close()
 
 
@@ -48,19 +40,11 @@ if __name__ == '__main__':
 	threads = [Thread(target = make_query_request) for _ in range(100)]
 	other_threads = [Thread(target = make_order_request) for _ in range(100)]
 	threads.extend(other_threads)
-	# threads = [Thread(target = make_order_request) for _ in range(200)]
 
 	for thread in threads:
 		thread.start()
 	for thread in threads:
 		thread.join()
-
-	# processes = [Process(target = make_query_request) for _ in range(200)]
-	# for process in processes:
-	# 	process.start()
-	# for process in processes:
-	# 	process.join()
-
 
 	end_time = datetime.now();
 	print 'Running time: ' + str((end_time - start_time).microseconds) + ' microseconds'
--- test cases/fine_grained_server.py
+++ test cases/fine_grained_server.py
@@ -8,7 +8,6 @@ HOST_NAME = 'localhost'
 PORT_NUMBER = 8080
 file_dir = os.getcwd() + '/serverfiles/'
 log = []
-# logLock = threading.RLock()
 flights_schedule = {}
 flights_schedule_lock = threading.RLock()
 
@@ -51,10 +50,6 @@ def get_flights(filename):
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 	pass
 
-# sequential server
-# class ThreadedHTTPServer(HTTPServer):
-# 	pass
-
 class eachLog:
 	ipaddress = ''
 	dateandtime = ''
@@ -67,42 +62,27 @@ class MyHandler(BaseHTTPRequestHandler):
 		self.end_headers()
 
 	def do_GET(self):
-		# file_dir = 'C:/Dropbox/Courses/Concurrent Programming/project/webserver/files/'
-		# f = open(file_dir + self.path)
 		self.send_response(200)
 		self.send_header("Content-type", "text-html")
 		self.end_headers()
-		# self.wfile.write("Entered GET request handler --- ")
-		# self.wfile.write(f.read())
 		query = self.path.split('-')
-		# self.wfile.write(flights_schedule[query[0]].schedule[query[1]])
+
 		# create log
-		# mylog = eachLog()
-		# mylog.ipaddress = self.client_address[0]
-		# mylog.dateandtime = self.log_date_time_string()
-		# mylog.requiredfile = self.path
-		# # append log
-		# # logLock.acquire()
-		# log.append(mylog)
-		# logLock.release()
+		mylog = eachLog()
+		mylog.ipaddress = self.client_address[0]
+		mylog.dateandtime = self.log_date_time_string()
+		mylog.requiredfile = self.path
+		log.append(mylog)
+
 		if query[0] == 'Query':
 			flight_info = flights_schedule[query[1]].schedule_info[query[2]]
 			res = ' '
 			self.wfile.write(flight_info)
-			# time.sleep(0.3)
 		if query[0] == 'Order':
-			# flights_schedule_lock.acquire()
-			# try:
 			res = flights_schedule[query[1]].add_reservation(query[2])
-				# flight_info = flights_schedule[query[1]].schedule_info[query[2]]
 			self.wfile.write(res)
-				# time.sleep(0.3)
-			# finally:
-			# 	flights_schedule_lock.release()
-		# self.wfile.write("Sending response!")
 
 if __name__ == '__main__':
-	# test()
 	# get flight dict
 	flights_schedule = get_flights('flights.csv')
 	# print the schedule info
@@ -118,11 +98,8 @@ if __name__ == '__main__':
 	except KeyboardInterrupt:
 		print '\n'
 		for i in log:
-		#print i.ipaddress
 			print 'log:'+'  '+i.ipaddress+'  '+i.dateandtime+'  '+i.requiredfile
-			#print 'log:'+'  '+i.ipaddress+'  '+i.requiredfile+'\t\n'
 	print '\n'
 	for key in flights_schedule.keys():
 		print key + " " + str(flights_schedule[key].schedule_info)
 	my_server.server_close()
-	# print flights_schedule['UA001']['Mon']
